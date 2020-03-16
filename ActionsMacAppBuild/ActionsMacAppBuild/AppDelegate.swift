72bd1d825c0e79f6480ec892e7c5767f9b436d76
DIO0550 <diot12345@yahoo.co.jp>
2020-03-16 20:57:29 +0900

    空のプロジェクト作成

---
+++ ActionsMacAppBuild/ActionsMacAppBuild/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ActionsMacAppBuild
+//
+//  Created by DIO on 2020/03/16.
+//  Copyright © 2020 DIO. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
